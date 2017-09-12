d8a9c9afe57be30efca0332e4b2aa3af97b4197f
Pawandeep Singh <pawandeep2155@gmail.com>
2017-09-13 00:23:09 +0530

    Add new Branch

---
+++ bayes_covariance_same.m
@@ -0,0 +1,18 @@
+% Read Data as a Matrix from text file
+data = load('3_ls.txt');
+
+% Calculate Mean and covariance for each class in data. First 500 rows
+% class w1 data, next 500 rows class w2 data, remaining 500 rows class w3
+% data
+num_of_rows = size(data,1);
+class_rows = uint16(size(data,1)/3);
+
+mean_class_1 = mean_calculate(data(1:class_rows,:));
+mean_class_2 = mean_calculate(data(class_rows+1:2*class_rows,:));
+mean_class_3 = mean_calculate(data(2*class_rows+1:num_of_rows,:));
+
+covariance_class_1 = covariance_calculate(data(1:class_rows,:),mean_class_1);
+covariance_class_2 = covariance_calculate(data(class_rows+1:2*class_rows,:),mean_class_2);
+covariance_class_3 = covariance_calculate(data(2*class_rows+1:num_of_rows,:),mean_class_3);
+
+
--- covariance_calculate.m
+++ covariance_calculate.m
@@ -1,9 +1,9 @@
 function covariance = covariance_calculate(input_matrix,mean_matrix)
 
-    covariance = zeros(size(input_matrix,1),size(input_matrix,1));
+    covariance = zeros(size(mean_matrix,1),size(mean_matrix,1));
 
     for i=1:size(input_matrix,1)
-        covariance = covariance + (input_matrix(:,i) - mean_matrix(:,1))*(input_matrix(:,i) - mean_matrix(:,1))';
+        covariance = covariance + (input_matrix(i,:) - mean_matrix(:,1))*(input_matrix(i,:) - mean_matrix(:,1))';
     end
     
     covariance = covariance/size(input_matrix,1);
