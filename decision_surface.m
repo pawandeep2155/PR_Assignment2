ad0faae3466ab9e307003d60d145270d19759d72
ajay532 <ajaypandey1996@gmail.com>
2017-09-19 21:50:18 +0530

    Decision Surface, ROC started

--- decision_surface.m
+++ decision_surface.m
@@ -6,25 +6,63 @@ w1=zeros(150,2);
 w2=zeros(150,2);
 w3=zeros(150,2);
 for i=351:500    
-w1(i,:)=a(i,:);
+w1(i-350,:)=a(i,:);
 end
 scatter(w1(:,1),w1(:,2),25,'filled');
 hold on
 
 for i=851:1000    
-w2(i,:)=a(i,:);
+w2(i-850,:)=a(i,:);
 end
 scatter(w2(:,1),w2(:,2),25,'filled');
 
-for i=1350:1500
-w3(i,:)=a(i,:);
+for i=1351:1500
+w3(i-1350,:)=a(i,:);
 end
 scatter(w3(:,1),w3(:,2),25,'filled');
 
 
-% 1 vs all
-t1=w1;
-t2=[w2;w3];
+% % 1 vs all
+% t1=w1;
+% t2=[w2;w3];
+% u1=mean_DS(t1);
+% cov1=covariance_calculate(t1,u1);
+% A1=-0.5*inv(cov1);
+% B1=inv(cov1)*u1;
+% C1=-(0.5)*u1'*inv(cov1)*u1-0.5*log(det(cov1));
+% 
+% u2=mean_DS(t2);
+% cov2=covariance_calculate(t2,u2);
+% A2=-0.5*inv(cov2);
+% B2=inv(cov2)*u2;
+% C2=-(0.5)*u2'*inv(cov2)*u2-0.5*log(det(cov2));
+% 
+% f=@(x,y) x.^2.*(A1(1,1)-A2(1,1))+y.^2.*(A1(2,2)-A2(2,2))+x.*y.*(A1(1,2)+A1(2,1)-A2(1,2)-A2(2,1))+x.*(B1(1,1)-B2(1,1))+y.*(B1(2,1)-B2(2,1))+C1-C2;
+% fimplicit(f);
+
+% 2 vs all
+t1=w2;
+t2=[w1;w3];
+u1=mean_DS(t1);
+cov1=covariance_calculate(t1,u1);
+A1=-0.5*inv(cov1);
+B1=inv(cov1)*u1;
+C1=-(0.5)*u1'*inv(cov1)*u1-0.5*log(det(cov1));
+
+u2=mean_DS(t2);
+cov2=covariance_calculate(t2,u2);
+A2=-0.5*inv(cov2);
+B2=inv(cov2)*u2;
+C2=-(0.5)*u2'*inv(cov2)*u2-0.5*log(det(cov2));
+
+f1=@(x,y) x.^2.*(A1(1,1)-A2(1,1))+y.^2.*(A1(2,2)-A2(2,2))+x.*y.*(A1(1,2)+A1(2,1)-A2(1,2)-A2(2,1))+x.*(B1(1,1)-B2(1,1))+y.*(B1(2,1)-B2(2,1))+C1-C2;
+fimplicit(f1,'LineWidth',2);
+
+
+
+% 3 vs all
+t1=w3;
+t2=[w1;w2];
 u1=mean_DS(t1);
 cov1=covariance_calculate(t1,u1);
 A1=-0.5*inv(cov1);
@@ -38,44 +76,15 @@ B2=inv(cov2)*u2;
 C2=-(0.5)*u2'*inv(cov2)*u2-0.5*log(det(cov2));
 
 f=@(x,y) x.^2.*(A1(1,1)-A2(1,1))+y.^2.*(A1(2,2)-A2(2,2))+x.*y.*(A1(1,2)+A1(2,1)-A2(1,2)-A2(2,1))+x.*(B1(1,1)-B2(1,1))+y.*(B1(2,1)-B2(2,1))+C1-C2;
-fimplicit(f ,[-10 10 -10 10]);
+fimplicit(f,'LineWidth',2);
 
-% % % 2 vs all
-% % t1=w2;
-% % t2=[w1;w3];
-% % u1=mean_DS(t1);
-% % cov1=covariance_calculate(t1,u1);
-% % A1=-0.5*inv(cov1);
-% % B1=inv(cov1)*u1;
-% % C1=-(0.5)*u1'*inv(cov1)*u1-0.5*log(det(cov1));
-% % 
-% % u2=mean_DS(t2);
-% % cov2=covariance_calculate(t2,u2);
-% % A2=-0.5*inv(cov2);
-% % B2=inv(cov2)*u2;
-% % C2=-(0.5)*u2'*inv(cov2)*u2-0.5*log(det(cov2));
-% % 
-% % f1=@(x,y) x.^2.*(A1(1,1)-A2(1,1))+y.^2.*(A1(2,2)-A2(2,2))+x.*y.*(A1(1,2)+A1(2,1)-A2(1,2)-A2(2,1))+x.*(B1(1,1)-B2(1,1))+y.*(B1(2,1)-B2(2,1))+C1-C2;
-% % fimplicit(f1 ,[-10 10 -10 10]);
-% % 
-% % % % 3 vs all
-% % % t1=w3;
-% % % t2=[w1;w2];
-% % % u1=mean_DS(t1);
-% % % cov1=covariance_calculate(t1,u1);
-% % % A1=-0.5*inv(cov1);
-% % % B1=inv(cov1)*u1;
-% % % C1=-(0.5)*u1'*inv(cov1)*u1-0.5*log(det(cov1))+log(pc1);
-% % % 
-% % % u2=mean_DS(t2);
-% % % cov2=covariance_calculate(t2,u2);
-% % % A2=-0.5*inv(cov2);
-% % % B2=inv(cov2)*u2;
-% % % C2=-(0.5)*u2'*inv(cov2)*u2-0.5*log(det(cov2))+log(pc2);
-% % % 
-% % % f=@(x,y) x.^2.*(A1(1,1)-A2(1,1))+y.^2.*(A1(2,2)-A2(2,2))+x.*y.*(A1(1,2)+A1(2,1)-A2(1,2)-A2(2,1))+x.*(B1(1,1)-B2(1,1))+y.*(B1(2,1)-B2(2,1))+C1-C2;
-% % % fimplicit(f);
 
+
+hold off
+legend({'Class 1','Class 2','Class 3'},'FontSize',12,'FontWeight','bold');
+xlabel('X1','FontSize',12,'FontWeight','bold');
+ylabel('X2','FontSize',12,'FontWeight','bold');
+title('Decision Surface', 'FontSize',12);
 % u2=;
 % v2=;
 % x=[1,2];
