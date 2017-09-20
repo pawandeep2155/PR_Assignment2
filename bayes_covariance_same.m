84418b12da627175f0ee47ff5dada5c55c68cbf7
Pawandeep Singh <pawandeep2155@gmail.com>
2017-09-20 13:56:42 +0530

    ROC and report started

--- bayes_covariance_same.m
+++ bayes_covariance_same.m
@@ -1,7 +1,10 @@
 %% Find mean and covariance for all the three classes
 
 %Read Real Data as a Matrix from text file
-data = load('3_ls.txt');
+data1 = load('class1.txt');
+data2 = load('class2.txt');
+data3 = load('class3.txt');
+data = [data1;data2;data3];
 
 % Calculate Mean and covariance for each class in data. First 500 rows
 % class w1 data, next 500 rows class w2 data, remaining 500 rows class w3
@@ -117,6 +120,122 @@ covariance_class_3 = covariance_calculate(data(2*class_rows+1:num_of_rows,:),mea
 % set(gca,'fontsize',18);
 
 %% Draw Eigen Vector and contour
+% [x,y] = meshgrid(-10:.2:25,-15:.2:20);
+% x1 = zeros(size(x,1) * size(x,1),1);
+% y1 = x1;
+% l = 1;
+% 
+% for i=1:size(x,1)
+%     for j=1:size(x,1)
+%         x1(l,1) = x(i,j);
+%         y1(l,1) = y(i,j);
+%         l = l+1;
+%     end
+% end
+% 
+% Contour
+% clf;
+% figure(1);
+% z1 = [x1,y1];
+% clf;
+% 
+% z2 = calculate_gaussian(covariance_class_1,mean_class_1,z1);
+% z = vec2mat(z2,size(x,1));
+% contour(x,y,z,'LineWidth',8);
+% hold on;
+% z3 = calculate_gaussian(covariance_class_1,mean_class_2,z1);
+% z = vec2mat(z3,size(x,1));
+% contour(x,y,z,'LineWidth',8);
+% z4 = calculate_gaussian(covariance_class_1,mean_class_3,z1);
+% z = vec2mat(z4,size(x,1));
+% contour(x,y,z,'LineWidth',8);
+% hold on;
+% 
+% Eigen Vector
+% [eigen_vector,eigen_value1] = eig(covariance_class_1);
+% eigen_vector = eigen_vector.*20;
+% 
+% plot([mean_class_1(1) eigen_vector(1,1)],[mean_class_1(2) eigen_vector(2,1)],'LineWidth',6);
+% plot([mean_class_1(1) eigen_vector(1,2)],[mean_class_1(2) eigen_vector(2,2)],'LineWidth',6);
+% 
+% [eigen_vector,eigen_value2] = eig(covariance_class_2);
+% eigen_vector = eigen_vector.*20;
+% 
+% plot([mean_class_2(1) eigen_vector(1,1)],[mean_class_2(2) eigen_vector(2,1)],'LineWidth',6);
+% plot([mean_class_2(1) eigen_vector(1,2)],[mean_class_2(2) eigen_vector(2,2)],'LineWidth',6);
+% 
+% [eigen_vector,eigen_value] = eig(covariance_class_3);
+% eigen_vector = eigen_vector.*20;
+% plot([mean_class_3(1) eigen_vector(1,1)],[mean_class_3(2) eigen_vector(2,1)],'LineWidth',6);
+% plot([mean_class_3(1) eigen_vector(1,2)],[mean_class_3(2) eigen_vector(2,2)],'LineWidth',6);
+% 
+% title('Constant Density Curve and Eigen Vectors');
+% xlabel('Feature 1');
+% ylabel('Feature 2');
+% set(gca,'fontsize',18);
+
+%% Plot decision surface & Contour
+
+w1=zeros(150,2);
+w2=zeros(150,2);
+w3=zeros(150,2);
+for i=351:500    
+w1(i-350,:)=data(i,:);
+end
+
+for i=851:1000    
+w2(i-850,:)=data(i,:);
+end
+
+for i=1351:1500
+w3(i-1350,:)=data(i,:);
+end
+
+%1 vs all
+t1=w1;
+t2=[w2;w3];
+u1=mean_DS(t1);
+cov1=covariance_calculate(t1,u1);
+A1=-0.5*inv(cov1);
+B1=inv(cov1)*u1;
+C1=-(0.5)*u1'*inv(cov1)*u1-0.5*log(det(cov1));
+u2=mean_DS(t2);
+cov2=covariance_calculate(t2,u2);
+A2=-0.5*inv(cov2);
+B2=inv(cov2)*u2;
+C2=-(0.5)*u2'*inv(cov2)*u2-0.5*log(det(cov2));
+f=@(x,y) x.^2.*(A1(1,1)-A2(1,1))+y.^2.*(A1(2,2)-A2(2,2))+x.*y.*(A1(1,2)+A1(2,1)-A2(1,2)-A2(2,1))+x.*(B1(1,1)-B2(1,1))+y.*(B1(2,1)-B2(2,1))+C1-C2;
+
+%2 vs all
+t1=w2;
+t2=[w1;w3];
+u1=mean_DS(t1);
+cov1=covariance_calculate(t1,u1);
+A1=-0.5*inv(cov1);
+B1=inv(cov1)*u1;
+C1=-(0.5)*u1'*inv(cov1)*u1-0.5*log(det(cov1));
+u2=mean_DS(t2);
+cov2=covariance_calculate(t2,u2);
+A2=-0.5*inv(cov2);
+B2=inv(cov2)*u2;
+C2=-(0.5)*u2'*inv(cov2)*u2-0.5*log(det(cov2));
+f1=@(x,y) x.^2.*(A1(1,1)-A2(1,1))+y.^2.*(A1(2,2)-A2(2,2))+x.*y.*(A1(1,2)+A1(2,1)-A2(1,2)-A2(2,1))+x.*(B1(1,1)-B2(1,1))+y.*(B1(2,1)-B2(2,1))+C1-C2;
+
+%3 vs all
+t1=w3;
+t2=[w1;w2];
+u1=mean_DS(t1);
+cov1=covariance_calculate(t1,u1);
+A1=-0.5*inv(cov1);
+B1=inv(cov1)*u1;
+C1=-(0.5)*u1'*inv(cov1)*u1-0.5*log(det(cov1));
+u2=mean_DS(t2);
+cov2=covariance_calculate(t2,u2);
+A2=-0.5*inv(cov2);
+B2=inv(cov2)*u2;
+C2=-(0.5)*u2'*inv(cov2)*u2-0.5*log(det(cov2));
+f2=@(x,y) x.^2.*(A1(1,1)-A2(1,1))+y.^2.*(A1(2,2)-A2(2,2))+x.*y.*(A1(1,2)+A1(2,1)-A2(1,2)-A2(2,1))+x.*(B1(1,1)-B2(1,1))+y.*(B1(2,1)-B2(2,1))+C1-C2;
+
 [x,y] = meshgrid(-10:.2:25,-15:.2:20);
 x1 = zeros(size(x,1) * size(x,1),1);
 y1 = x1;
@@ -130,45 +249,29 @@ for i=1:size(x,1)
     end
 end
 
-%Contour
-clf;
-figure(1);
 z1 = [x1,y1];
-clf;
-
 z2 = calculate_gaussian(covariance_class_1,mean_class_1,z1);
 z = vec2mat(z2,size(x,1));
-contour(x,y,z,'LineWidth',8);
+%contour(x,y,z,'LineWidth',4);
 hold on;
-z3 = calculate_gaussian(covariance_class_1,mean_class_2,z1);
+z3 = calculate_gaussian(covariance_class_2,mean_class_2,z1);
 z = vec2mat(z3,size(x,1));
-contour(x,y,z,'LineWidth',8);
-z4 = calculate_gaussian(covariance_class_1,mean_class_3,z1);
+%contour(x,y,z,'LineWidth',4);
+
+z4 = calculate_gaussian(covariance_class_3,mean_class_3,z1);
 z = vec2mat(z4,size(x,1));
-contour(x,y,z,'LineWidth',8);
-hold on;
+%contour(x,y,z,'LineWidth',4);
 
-%Eigen Vector
-[eigen_vector,eigen_value1] = eig(covariance_class_1);
-eigen_vector = eigen_vector.*20;
+fimplicit(f,'LineWidth',2);
+fimplicit(f1,'LineWidth',2);
+fimplicit(f2,'LineWidth',2);
+h1 = scatter(w1(:,1),w1(:,2),25,'filled');
+h2 = scatter(w2(:,1),w2(:,2),25,'filled');
+h3 = scatter(w3(:,1),w3(:,2),25,'filled');
 
-plot([mean_class_1(1) eigen_vector(1,1)],[mean_class_1(2) eigen_vector(2,1)],'LineWidth',6);
-plot([mean_class_1(1) eigen_vector(1,2)],[mean_class_1(2) eigen_vector(2,2)],'LineWidth',6);
-
-[eigen_vector,eigen_value2] = eig(covariance_class_2);
-eigen_vector = eigen_vector.*20;
-
-plot([mean_class_2(1) eigen_vector(1,1)],[mean_class_2(2) eigen_vector(2,1)],'LineWidth',6);
-plot([mean_class_2(1) eigen_vector(1,2)],[mean_class_2(2) eigen_vector(2,2)],'LineWidth',6);
-
-[eigen_vector,eigen_value] = eig(covariance_class_3);
-eigen_vector = eigen_vector.*20;
-plot([mean_class_3(1) eigen_vector(1,1)],[mean_class_3(2) eigen_vector(2,1)],'LineWidth',6);
-plot([mean_class_3(1) eigen_vector(1,2)],[mean_class_3(2) eigen_vector(2,2)],'LineWidth',6);
-
-title('Constant Density Curve and Eigen Vectors');
-xlabel('Feature 1');
-ylabel('Feature 2');
-set(gca,'fontsize',18);
+legend([h1 h2 h3],{'Class 1','Class 2', 'Class 3'},'FontSize',18,'FontWeight','bold');
+xlabel('Feature1','FontSize',18,'FontWeight','bold');
+ylabel('Feature2','FontSize',18,'FontWeight','bold');
+title('Decision Surface', 'FontSize',18);
 
 
